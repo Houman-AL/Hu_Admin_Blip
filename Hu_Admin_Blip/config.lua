36eafc077c4f06b9080bb4ec4f9eaf88739fff1b
Houman AL <82032044+Houman-AL@users.noreply.github.com>
2021-04-17 16:19:52 +0430

    Add files via upload

---
+++ Hu_Admin_Blip/config.lua
@@ -0,0 +1,20 @@
+
+--[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+                --[ Houman ]]--                 
+--[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+
+Config = {}
+
+Config.SetAdminBlipPerm = 1
+
+Config.ToggleAdminBlipCommand = 'toggleadmin'
+
+Config.ToggleAdminBlipInfo = 'toggle admin in map'
+
+Config.blipid = 304 -- you can use https://wiki.rage.mp/index.php?title=Blips
+
+Config.blipcolor = 1 -- you can use https://wiki.rage.mp/index.php?title=Blips
+
+--[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+                --[ Houman ]]--                 
+--[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
